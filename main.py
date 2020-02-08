233f70cd8f1178d174a1c0c84e304a051b190fa9
kevinliu9 <56597450+kevinliu9@users.noreply.github.com>
2020-02-08 16:29:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,18 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World! Goodbye World!'
+    return '''                                 Hello Person!
+                                  Please type in a fake news source here:
+                                  
+                     ----------------------------------------------------------------
+                    |                                                                |
+                    |                                                                |
+                     ----------------------------------------------------------------
+                     
+                                       Thanks for playing! :)
+                                       
+                               Made by Ian, Brendan, Gordon, Kevin
+           '''
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
